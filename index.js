bdf71797487fded123882d773acfda8217987efc
chris.yu <yuetong3yu@gmail.com>
2024-01-27 23:53:28 +1100

    1. Customized deploy to aws

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+const serverless = require('serverless-http')
+const express = require('express')
+const app = express()
 
-app.get("/", (req, res, next) => {
+app.get('/', (req, res) => {
   return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/path", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
+    content: 'Hello from Chris!',
+  })
+})
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
-  });
-});
+    error: 'Not Found',
+  })
+})
 
-module.exports.handler = serverless(app);
+module.exports.hello = serverless(app)
